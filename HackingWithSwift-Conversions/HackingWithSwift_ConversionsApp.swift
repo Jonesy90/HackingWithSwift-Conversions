4ccea2550b97afc1988915eb619c13b7bbd9bec2
Michael Jones <michael.jones90@me.com>
2026-05-25 17:50:23 +0100

    Initial Commit

---
+++ HackingWithSwift-Conversions/HackingWithSwift_ConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwift_ConversionsApp.swift
+//  HackingWithSwift-Conversions
+//
+//  Created by Michael Jones on 25/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwift_ConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
