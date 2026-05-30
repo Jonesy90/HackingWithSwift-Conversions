99894a64d0bb02065aa569f94fae1a639033683c
Michael Jones <michael.jones90@me.com>
2026-05-30 13:47:11 +0100

    - SwiftUI layout added. - convertedValue computed property. This checks what the 'fromUnit' is and converts it to 'Celsius'. - outputValue computed property. Converts the inputValue (which is converted to Celsius) and converts it to the requested Unit.

--- HackingWithSwift-Conversions/ContentView.swift
+++ HackingWithSwift-Conversions/ContentView.swift
@@ -8,14 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    @State private var fromUnit: String = "Celsius"
+    @State private var toUnit: String = "Fahrenheit"
+    @State private var inputValue: Double = 0
+    
+    var temperatureUnits = ["Celsius", "Fahrenheit", "Kelvin"]
+    
+    var convertedValue: Double {
+        var value: Double = 0
+        
+        if fromUnit == "Celsius" {
+            value = inputValue
+        } else if fromUnit == "Fahrenheit" {
+            value =  (inputValue - 32) * 5 / 9
+        } else if fromUnit == "Kelvin" {
+            value = (inputValue - 273.15)
         }
-        .padding()
+        
+        return value
+    }
+    
+    var outputValue: Double {
+        var outputValue: Double = 0
+        
+        if toUnit == "Celsius" {
+            outputValue = convertedValue
+        } else if toUnit == "Fahrenheit" {
+            outputValue = (convertedValue * 9 / 5) + 32
+        } else if toUnit == "Kelvin" {
+            outputValue = convertedValue + 273.15
+        }
+        
+        return outputValue
+    }
+    
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("Input Value") {
+                    TextField("Input Value", value: $inputValue, format: .number)
+                }
+                
+                Section("From Temperature") {
+                    Picker("From Temperature", selection: $fromUnit) {
+                        ForEach(temperatureUnits, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("To Temperature") {
+                    Picker("From Temperature", selection: $toUnit) {
+                        ForEach(temperatureUnits, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Output Value") {
+                    Text("\(outputValue.formatted())")
+                }
+                
+            }
+            .navigationTitle("Temperature Conversion")
+        }
     }
 }
 
